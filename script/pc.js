012a23731289d9f12b11901c1c3b69bc8e07cadf
yuffiy <yfhj1990@hotmail.com>
2016-05-31 11:01:43 +0800

    add electron builder and npm script

---
+++ script/pc.js
@@ -0,0 +1,38 @@
+var electron = require('electron')
+
+function main() {
+    
+    var win
+    
+    return function(app, BrowserWindow) {
+	function createWindow() {
+	    filepath = 'file://' + __dirname + '/../tmp/index.html'
+	    
+	    win = new BrowserWindow({ width: 800, heihgt: 600 })
+
+	    win.loadURL(filepath)
+
+	    win.webContents.openDevTools()
+
+	    win.on('closed', function() {
+		win = null
+	    })
+	}
+
+	app.on('ready', createWindow)
+
+	app.on('window-all-close', function() {
+	    if(process.platform !== 'darwin') app.quit()
+	})
+
+	app.on('activate', function() {
+	    if(win === null) createWindow()
+	})
+    }
+}
+
+
+var app           = electron.app
+var BrowserWindow = electron.BrowserWindow
+
+main()(app, BrowserWindow)
