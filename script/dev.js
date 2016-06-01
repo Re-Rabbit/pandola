7e77268813c17db36f73e0573d240350b61ce7ac
pl <0x00.pl@gmail.com>
2016-06-01 23:00:34 +0800

    fix merge

--- script/dev.js
+++ script/dev.js
@@ -1,15 +1,3 @@
-/**
- * Gulp file 项目构建文件
- *
- * 开发环境
- */
-<<<<<<< HEAD
-var gulp = require('gulp')
-var del = require('del')
-var html = require('./build-html.js')
-var style = require('./build-style.js')
-var paths = require('./paths.js')
-=======
 
 
 // @Todo add Javascript builder.
@@ -19,7 +7,6 @@ var del         = require('del')
 var html        = require('./build-html.js')
 var style       = require('./build-style.js')
 var paths       = require('./paths.js')
->>>>>>> 3085fe9d5bdda7a37207ba9d0b12709d8451478e
 var browserSync = require('browser-sync').create()
 
 /**
@@ -63,62 +50,35 @@ function main() {
 
     // reload files
     function reload(done) {
-<<<<<<< HEAD
         bsReload()
         done()
-=======
-	bsReload()
-	done()
->>>>>>> 3085fe9d5bdda7a37207ba9d0b12709d8451478e
     }
 
     // start server
     function server() {
-<<<<<<< HEAD
         browserSync.init({
             port: 8888,
             server: {
                 baseDir: paths.tmp
             }
         })
-=======
-	browserSync.init({
-	    port: 8888,
-            server: {
-		baseDir: paths.tmp
-            }
-	})
->>>>>>> 3085fe9d5bdda7a37207ba9d0b12709d8451478e
     }
 
     // watcher
     function watch() {
-<<<<<<< HEAD
         server()
-=======
-	server()
->>>>>>> 3085fe9d5bdda7a37207ba9d0b12709d8451478e
         gulp.watch(paths.dirs.html, gulp.series(buildHtml, reload))
         gulp.watch(paths.dirs.styleSources, gulp.series(buildStyle, reload))
     }
 
     // export
     gulp.task('default',
-<<<<<<< HEAD
-        gulp.series(clean
-            , gulp.parallel(buildHtml
-                , buildStyle
-            )
-            , watch
-        ))
-=======
               gulp.series( clean
                          , gulp.parallel( buildHtml
                                         , buildStyle
                                         )
 			 , watch
 			 ))
->>>>>>> 3085fe9d5bdda7a37207ba9d0b12709d8451478e
 }
 
 
