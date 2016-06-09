312072cc4fc8c7784dcbf183bb81a7030529ca37
Yufi <yfhj1990@hotmail.com>
2016-06-09 17:13:59 +0800

    update header style

--- src/scripts/pages/dashboard/icon.js
+++ src/scripts/pages/dashboard/icon.js
@@ -11,9 +11,9 @@ function replaceTpl(icon) {
 
 function appendToContainer(con) {
     return function(ctx) {
-	return function() {
-	    con.innerHTML = ctx
-	}
+	      return function() {
+	          con.innerHTML = ctx
+	      }
     }
 }
 
@@ -27,19 +27,19 @@ function filterErrorIcons(data) {
 
 function reMatch(re) {
     return function(ctx) {
-	return re.test(ctx)
+	      return re.test(ctx)
     }
 }
 
 function replaceAndConcatTpl(tplCall) {
     return function(data) {
-	return data.map(tplCall).join('')
+	      return data.map(tplCall).join('')
     }
 }
 
 function findMatchedIcon(call) {
     return function(data) {
-	return data.filter(res => call(res.name))
+	      return data.filter(res => call(res.name))
     }
 }
 
@@ -51,38 +51,40 @@ function main() {
 
     fetch(api('icon'))
         .then(toJson)
-	.then(filterErrorIcons)
-	.then(res => {
-	    function eventBinding(evt) {
-		let value = evt.target.value.trim()
-		let reMatchValue = new RegExp(`${value}`)
-		let tpls = replaceAndConcatTpl(replaceTpl)
-		let finder = findMatchedIcon(reMatch(reMatchValue))
-		
-		let iconTpls = value === '' ? tpls(res) : tpls(finder(res))
+	      .then(filterErrorIcons)
+	      .then(res => {
+	          function eventBinding(evt) {
+		            let value = evt.target.value.trim()
+		            let reMatchValue = new RegExp(`${value}`)
+		            let tpls = replaceAndConcatTpl(replaceTpl)
+		            let finder = findMatchedIcon(reMatch(reMatchValue))
 
-		requestAnimationFrame(appendToContainer(container)(iconTpls))
-	    }
-	    
-	    
-	    search.addEventListener('keyup', eventBinding)
-	    search.dispatchEvent(new KeyboardEvent('keyup'))
+		            let iconTpls = value === '' ? tpls(res) : tpls(finder(res))
 
-	    let clipboard = new Clipboard('.icons__item')
-	    
-	    clipboard.on('success', function(e) {
+		            requestAnimationFrame(appendToContainer(container)(iconTpls))
 
-		notify.of({ position: 'bm', state: 'success', content: `已复制 ${e.text} 到剪贴板` })
 
-		e.clearSelection()
-	    })
+                document.querySelector('#count').innerHTML = res.length
+	          }
 
-	    clipboard.on('error', function(e) {
-		notify.of({ state: 'error', content: `复制失败` })
-	    });
 
-	    
-	})
+	          search.addEventListener('keyup', eventBinding)
+	          search.dispatchEvent(new KeyboardEvent('keyup'))
+
+	          let clipboard = new Clipboard('.icon')
+
+	          clipboard.on('success', function(e) {
+
+		            notify.of({ position: 'bm', state: 'success', content: `已复制 ${e.text} 到剪贴板` })
+
+		            e.clearSelection()
+	          })
+
+	          clipboard.on('error', function(e) {
+		            notify.of({ state: 'error', content: `复制失败` })
+	          });
+
+	      })
 }
 
 main()
