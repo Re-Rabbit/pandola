6210032de68a517f92d9850fb9e9fc75c8e0ec0e
yuffiy <yfhj1990@hotmail.com>
2016-06-16 18:59:13 +0800

    update tool.

--- script/builder.js
+++ script/builder.js
@@ -15,6 +15,11 @@
  * @warning Development environment only.
  * @todo Add production environment builder.
  * @todo Check tmp dir is exist.
+ * @todo nunjucks template error then can't auto reload.
+ * @todo add webpack toString interface.
+ * @todo add init pages build for history operation.
+ * @todo need use a public path searcher.
+ * @todo fix nunjucks paths.
  */
 
 
@@ -23,14 +28,21 @@ const sass = require('gulp-sass')
 const nunjucks = require('gulp-nunjucks')
 const data = require('gulp-data')
 const tool = require('gulp-util')
-const nunjucks_impl = require('nunjucks')
+const Nunjucks = require('nunjucks')
 const named = require('vinyl-named')
 const webpack = require('webpack-stream')
 const del = require('del')
-const reload = require('browser-sync').reload
+const browserSync = require('browser-sync').create()
+//const reload = require('browser-sync').reload
 const webpackConfig = require('./../webpack.config.js')
 const path = require('path')
 const fs = require('fs')
+const chalk = require('chalk')
+
+const logger = require('./log.js')
+const log = logger.log
+const flag = logger.flag
+const pad = logger.pad
 
 /**
  * Paths
@@ -43,12 +55,19 @@ const libs = 'libs'
 // dest
 const tmp = 'tmp'
 // workspace
-var workspace = 'zhihu'
+var workspace = null
 const workspaceTmp = '.workspace'
 const workspaceTmpFile = `${tmp}/${workspaceTmp}`
 
-const stylesBuildPath = (workspace) => `${pages}/${workspace}/index.scss`
-const stylesWatchPath = () => [pages, components, libs].map(n => `${n}/**/[^#]*.scss`)
+const stylesBuildPath = (workspace) => `${pages}/${workspace ? workspace : '**'}/index.scss`
+const stylesWatchPath = () => [pages, components, libs].map(n => `${n}/**/[^#~]*.scss`)
+
+const htmlsBuildPath = (workspace) => `${pages}/${workspace ? workspace : '**'}/index.html`
+const htmlsWatchPath = () => [pages, components, libs].map(n => `${n}/**/[^#~]*.html`)
+
+const scriptsBuildPath = (workspace) => `${pages}/${workspace ? workspace : '**'}/index.js`
+const scriptsWatchPath = () => [pages, components, libs].map(n => `${n}/**/[^#~]*.js`)
+
 
 function clean() {
     return del('tmp/*')
@@ -65,10 +84,7 @@ function setWorkSpace(value) {
         const cb = typeof done === 'function' ? done : function() {}
         fs.writeFile(workspaceTmpFile, value, err => {
             if(err) throw err
-            console.log(tool.colors.bgMagenta(' Workspace '),
-                     tool.colors.green(`${workspace}`),
-                     '->',
-                     tool.colors.yellow(`${value}`))
+	    log.workspace(value)
             workspace = value
             cb()
         })
@@ -87,38 +103,125 @@ function defineSassImporter(filter) {
 
 function sassConfig() {
     return {
-	      outputStyle: 'expanded',
-	      indentType: 'space',
-	      importer: defineSassImporter(replaceScssComponentPath)
+	outputStyle: 'expanded',
+	indentType: 'space',
+	importer: defineSassImporter(replaceScssComponentPath)
     }
 }
 
 function css() {
     const cssPath = stylesBuildPath(workspace)
-    console.log(tool.colors.bgBlue(' CSS '), cssPath, +new Date)
+    
+    const start = +new Date
     return gulp
-	      .src(cssPath)
-	      .pipe(sass(sassConfig()))
-	      .pipe(gulp.dest(tmp))
+	.src(cssPath, { base: pages })
+	.pipe(sass(sassConfig()))
+	.pipe(gulp.dest(tmp))
+	.pipe(data(() => {
+	    log.css(workspace, +new Date - start)
+	}))
 }
 
-function watch(done) {
+function nunjucksEnv() {
+    return new Nunjucks.Environment(
+	new Nunjucks.FileSystemLoader([pages,
+				       `${pages}/${workspace}`,
+				       libs,
+				       components])
+    )
+}
 
-    gulp.watch(stylesWatchPath(), gulp.series(css))
-	      .on('change', (path, stats) => {
-	          if (stats)
-		            setImmediate(
-		                () => console.log(`${tool.colors.bgWhite(' File ')} ${tool.colors.magenta(path)} changed size to ${stats.size}`)
-		            )
-	      })
+function html() {
+    const htmlPath = htmlsBuildPath(workspace)
+    const start = +new Date
+    return gulp
+	.src(htmlPath, { base: pages })
+	.pipe(nunjucks
+	      .compile({}, { env: nunjucksEnv() })
+	      .on('error', console.log))
+	.pipe(gulp.dest(tmp))
+	.pipe(data(() => {
+	    log.html(workspace, +new Date - start)
+	}))
+}
 
+function reload(done) {
+    browserSync.reload()
+    done()
+}
 
-    gulp.watch(workspaceTmpFile, gulp.series(css))
+function javascriptServer(done) {
+    const jsPath = scriptsBuildPath(workspace)
+    
+    return gulp
+	.src(jsPath, { base: pages, allowEmpty: true })
+	.pipe(named())
+        .pipe(webpack(webpackConfig, null, (err, stats) => {
+	    const res = stats.toJson()
+	    log.js(workspace, res.time)
+	})).on('error', console.log)
+	.pipe(gulp.dest(tmp))
+}
+
+var historyApiFallback = require('connect-history-api-fallback')
+
+function browserServer(done) {
+
+    browserSync.init({
+        port: 8888,
+	//ui: false,
+	//logLevel: 'silent',
+	open: false,
+	reloadOnRestart: true,
+        startPath: `/${workspace}/`,
+        server: {
+            baseDir: tmp
+        },
+	middleware: [(req, res, next) => {
+	    const test = req.url.match(/^\/(\w+)\/$/)
+	    console.log(test, req.url)
+	    if(test && test[1] != workspace) {
+		setWorkSpace(test[1])(browserSync.reload)
+	    }
+
+	    // Disable cache because when browser history back which can't trigger setWorkSpace
+	    res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate, no-store')
+	    
+	    next()
+	}],
+    }, () => {
+	
+	log.browser(workspace, `http://localhost:8888/${workspace}/`)
+    })
+
+    browserSync.emitter.on('connent', () => {
+	console.log('1111111')
+    })
+    
+    gulp.watch(stylesWatchPath(), gulp.series(css, reload))
+	.on('change', (path, stats) => {
+	    log.file(`${chalk.yellow('@component')} ${path}`)
+	})
+
+    gulp.watch(htmlsWatchPath(), gulp.series(html, reload))
+	.on('change', (path, stats) => {
+	    log.file(`${chalk.yellow('@component')} ${path}`)
+	})
+
+    gulp.watch(scriptsWatchPath())
+	.on('change', (path, stats) => {
+	    log.file(`${chalk.yellow('@component')} ${path}`)
+	})
+
+    gulp.watch(workspaceTmpFile, gulp.series(css,
+					     html,
+					     reload))
 
     done()
 }
 
 
 gulp.task('default', gulp.series(clean,
-				                         gulp.parallel(watch,
+				 gulp.parallel(browserServer,
+					       javascriptServer,
                                                boot)))
